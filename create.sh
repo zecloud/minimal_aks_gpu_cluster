bc90077b9a8ed5b8a1469cd18d7331dd01d8a5d6
Aymeric Weinbach <397730+zecloud@users.noreply.github.com>
2023-05-21 10:08:21 +0200

    standard load balancer to have multiple pool nodes

--- create.sh
+++ create.sh
@@ -2,5 +2,5 @@ az aks create -n miniCluster \
     --resource-group minigpucluster \
     --node-count 1 \
     --node-vm-size Standard_B2s \
-    --load-balancer-sku basic \
+    --load-balancer-sku standard \
     --node-osdisk-size 32 
